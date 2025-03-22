1f650d9621d249ac5fbb326b42f14335163f8adf
Evelyn <crzycute4@gmail.com>
2025-03-23 02:50:36 +0800

    please

--- server.js
+++ server.js
@@ -40,5 +40,5 @@ app.delete('/channels/:id', (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
